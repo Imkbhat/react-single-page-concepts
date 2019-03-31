62b7da6a34cb64b7322842341672777a10705187
Kiran B S <kbs71190@gmail.com>
2019-03-31 10:59:35 +0530

    Add files via upload

---
+++ type-checking-with-proptypes.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Contact extends Component {
+  //Another appraoch to use propTypes
+  /*static propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired
+  };*/
+
+  render() {
+    const { name, email, phone } = this.props;
+    return (
+      <div>
+        <h4>{name}</h4>
+        <ul>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+//Best Appraoch
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
+
+export default Contact;
+
